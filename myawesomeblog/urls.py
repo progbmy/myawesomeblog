f2c00984b13222cea62b6ec17e277e597876eadb
MIhail <skuter63@mail.ru>
2020-12-05 00:28:10 +0400

    add homa page

--- myawesomeblog/urls.py
+++ myawesomeblog/urls.py
@@ -17,8 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings # импортируем управление настройками расположения
 from django.conf.urls.static import static # Импортируем управление урлами
-
+import events.views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', events.views.home, name = 'home'),
     ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
